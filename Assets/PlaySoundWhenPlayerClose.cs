0671545c242a559bd0bc77d481d27885aecca96e
simon.mackinnon <simon.mackinnon15@gmail.com>
2018-03-16 08:15:23 +1100

    Add script to play a sound when an object is close to the player, use this script to make skeleton hiss at you

---
+++ Assets/PlaySoundWhenPlayerClose.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundWhenPlayerClose : MonoBehaviour {
+
+    public float distanceToPlayer;
+    public bool hasApproached;
+    public AudioClip sound;
+
+    private AudioSource source;
+    
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        hasApproached = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.FindWithTag("Player");
+
+        distanceToPlayer = Vector2.Distance(player.transform.position, gameObject.transform.position);
+
+        if (distanceToPlayer < 5)
+        {
+            if (!hasApproached)
+            {
+                PlayEnemySound();
+            }
+            hasApproached = true;
+        }
+        else
+        {
+            hasApproached = false;
+        }
+
+    }
+
+    void PlayEnemySound()
+    {
+        source.PlayOneShot(sound, 1F);
+    }
+}
