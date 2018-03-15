1a238618bc1bc339111941de9412cb73d49a67e6
kthl <ikennyi@gmail.com>
2018-03-15 16:36:05 +1100

    Created slime monster enemy.

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public int EnemySpeed;
+    public int XMoveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
+
+        if(hit.distance < 0.7f)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        if(XMoveDirection > 0)
+        {
+            XMoveDirection = -1;
+        }
+        else
+        {
+            XMoveDirection = 1;
+        }
+    }
+}
