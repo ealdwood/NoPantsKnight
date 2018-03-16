e7afd6a02693704487a26d38c28e556db12141b0
simon.mackinnon <simon.mackinnon15@gmail.com>
2018-03-16 12:23:07 +1100

    Attach enemy working

---
+++ Assets/Scripts/SwordScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour {
+
+    public GameObject target;
+    public float attackDistance;
+
+    private Animator m_Anim;
+
+    void Awake()
+    {
+        m_Anim = GetComponent<Animator>();
+    }
+
+    void Update ()
+    {
+        if (m_Anim.GetBool("isAttacking"))
+        {
+            Attack();
+        }
+    }
+	
+    void Attack()
+    { 
+        if(target == null)
+        {
+            return;
+        }
+        float distance = Vector2.Distance(transform.position, target.transform.position);
+        bool facingEnemy = transform.localScale.x > 0
+                            ? target.transform.position.x > transform.position.x
+                            : target.transform.position.x < transform.position.x;
+
+        if (distance < attackDistance && facingEnemy)
+        {
+            BroadcastMessage("PlayKilledEnemysSound");
+            Destroy(target, 0.1F);
+        }          
+    }
+}
